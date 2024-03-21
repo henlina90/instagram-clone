5fa2a4ed710cade78ed4192da7442c538c4f01d3
Lina Hen <linahen1990@gmail.com>
2024-03-20 19:58:41 -0700

    home and auth page setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from '@chakra-ui/react'
+import { Route, Routes } from 'react-router-dom'
+import AuthPage from './pages/AuthPage/AuthPage'
+import HomePage from './pages/HomePage/HomePage'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/auth' element={<AuthPage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
