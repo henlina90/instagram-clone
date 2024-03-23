f486732b8b4129ce665e028b9fb5ae5c8db88540
Lina Hen <linahen1990@gmail.com>
2024-03-22 20:04:48 -0700

    start of implementing auth form and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { Button } from '@chakra-ui/react'
 import { Route, Routes } from 'react-router-dom'
+import PageLayout from './Layouts/PageLayout/PageLayout'
 import AuthPage from './pages/AuthPage/AuthPage'
 import HomePage from './pages/HomePage/HomePage'
 
 function App() {
   return (
-    <>
+    <PageLayout>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/auth' element={<AuthPage />} />
       </Routes>
-    </>
+    </PageLayout>
   )
 }
 
--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,8 +1,24 @@
 import React, { useState } from 'react'
-import {Box, VStack, Image, Input} from '@chakra-ui/react'
+import {Box, Flex, Text, Button, VStack, Image, Input} from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 
 const AuthForm = () => {
     const [isLogin, setIsLogin] = useState(true)
+    const navigate = useNavigate()
+    const [inputs, setInputs] = useState({
+        email:'',
+        password:'',
+        confirmPassword:''
+    })
+
+    const handleAuth = () => {
+        if (!inputs.email || !inputs.password) {
+            alert("Please fill all the fields")
+            return
+        }
+        navigate("/")
+    }
+
   return <>
       <Box border={"1px solid gray"} borderRadius={4} padding={5}>
         <VStack spacing={4}>
@@ -11,13 +27,55 @@ const AuthForm = () => {
                 placeholder='Email'
                 fontSize={14}
                 type='email'
+                value={inputs.email}
+                onChange={(e) => setInputs({...inputs,email:e.target.value})}
             />
             <Input 
                 placeholder='Password'
                 fontSize={14}
                 type='password'
+                value={inputs.password}
+                onChange={(e) => setInputs({...inputs,password:e.target.value})}
             />
+
+            {!isLogin? (
+                <Input
+                placeholder='Confirm Password'
+                value={inputs.confirmPassword}
+                onChange={(e) => setInputs({...inputs,confirmPassword:e.target.value})}
+                fontSize={14}
+                type='password' />
+            ) : null}
+
+            <Button w={"full"} colorScheme='blue' size={'sm'} fontSize={14} onClick={handleAuth}>
+                {isLogin? "Log in" : "Sign up"}
+            </Button>
+
+            <Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w={"full"}>
+                <Box flex={2} h={"1px"} bg={"gray.400"} />
+                <Text mx={1} color={"white"}>OR</Text>
+                <Box flex={2} h={"1px"} bg={"gray.400"} />
+            </Flex>
+
+            <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"}>
+                <Image src='/google.png' w={5} alt='Google logo' />
+                <Text mx="2" color={"blue.500"}>
+                    Log in with Google
+                </Text>
+            </Flex>
         </VStack>
+    </Box>
+
+    <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <Flex alignItems={"center"} justifyContent={"center"} >
+            <Box mx={2} fontSize={14}>
+
+                {isLogin ? "Don't have an account?" : "Already have an account?"}
+            </Box>
+            <Box onClick={() => setIsLogin(!isLogin)} color={'blue.500'} cursor={"pointer"}>
+                {isLogin ? "Log in" : "Sign up"}
+            </Box>
+        </Flex>
     </Box>
   </>
 }
